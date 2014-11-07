67e403717946a08102c8c151dee8a4c656698d3d
maciej <noocrat@gmail.com>
2014-11-07 00:58:01 +0000

    Use a Python object as a word-id mapping. Optimize moving window co-occurrence calculation. Add tests.

--- glove/corpus.py
+++ glove/corpus.py
@@ -1,6 +1,6 @@
 # Cooccurrence matrix construction tools
 # for fitting the GloVe model.
-
+import numpy as np
 try:
     # Python 2 compat
     import cPickle as pickle
@@ -14,12 +14,31 @@ class Corpus(object):
     """
     Class for constructing a cooccurrence matrix
     from a corpus.
+
+    A dictionry mapping words to ids can optionally
+    be supplied. If left None, it will be constructed
+    from the corpus.
     """
     
-    def __init__(self):
+    def __init__(self, dictionary=None):
 
-        self.dictionary = None
+        self.dictionary = {}
+        self.dictionary_supplied = False
         self.matrix = None
+
+        if dictionary is not None:
+            self._check_dict(dictionary)
+            self.dictionary = dictionary
+            self.dictionary_supplied = True
+
+    def _check_dict(self, dictionary):
+
+        if (np.max(list(dictionary.values())) != (len(dictionary) - 1)):
+            raise Exception('The largest id in the dictionary '
+                            'should be equal to its length minus one.')
+
+        if np.min(list(dictionary.values())) != 0:
+            raise Exception('Dictionary ids should start at zero')
 
     def fit(self, corpus, window=10):
         """
@@ -31,9 +50,11 @@ class Corpus(object):
         - int window: the length of the (symmetric)
           context window used for cooccurrence.
         """
-
-        self.dictionary, self.matrix = construct_cooccurrence_matrix(corpus, 
-                                                                     int(window))
+        
+        self.matrix = construct_cooccurrence_matrix(corpus,
+                                                    self.dictionary,
+                                                    int(self.dictionary_supplied),
+                                                    int(window))
 
     def save(self, filename):
         
---
+++ glove/tests/test_corpus.py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8 -*-
+from nose.tools import raises
+import numpy as np
+
+
+from glove import Corpus
+
+
+def test_corpus_construction():
+
+    corpus_words = ['a', 'naïve', 'fox']
+    corpus = [corpus_words]
+
+    model = Corpus()
+    model.fit(corpus, window=10)
+
+    for word in corpus_words:
+        assert word in model.dictionary
+
+    assert model.matrix.shape == (len(corpus_words),
+                                  len(corpus_words))
+
+    expected = [[0.0, 1.0, 0.5],
+                [0.0, 0.0, 1.0],
+                [0.0, 0.0, 0.0]]
+
+    assert (model.matrix.todense().tolist()
+            == expected)
+
+
+def test_supplied_dictionary():
+
+    dictionary = {'a': 2,
+                  'naïve': 1,
+                  'fox': 0}
+
+    corpus = [['a', 'naïve', 'fox']]
+
+    model = Corpus(dictionary=dictionary)
+    model.fit(corpus, window=10)
+
+    assert model.dictionary == dictionary
+
+    assert model.matrix.shape == (len(dictionary),
+                                  len(dictionary))
+
+    assert (model.matrix.tocsr()[2]).sum() == 0
+
+
+@raises(Exception)
+def test_supplied_dict_checks():
+
+    dictionary = {'a': 4,
+                  'naïve': 1,
+                  'fox': 0}
+
+    corpus = [['a', 'naïve', 'fox']]
+
+    model = Corpus(dictionary=dictionary)
+
+
+@raises(KeyError)
+def test_supplied_dict_missing():
+
+    dictionary = {'a': 1,
+                  'naïve': 0}
+
+    corpus = [['a', 'naïve', 'fox']]
+
+    model = Corpus(dictionary=dictionary)
+    model.fit(corpus, window=10)
